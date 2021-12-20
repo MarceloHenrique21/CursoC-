2723f15f4936bd7b4f2d7bc3b58dadaf7cd61036
Marcelo H <73535877+feitan02@users.noreply.github.com>
2021-12-20 13:40:18 -0300

    Update Usando IF.cs

--- Usando IF.cs
+++ Usando IF.cs
@@ -5,7 +5,16 @@ class arquivo1{
 
  public static void Main(){
 
-     Console.WriteLine("Estrutura base C#");
+      int A = 30;
+
+        if (A < 20 || A != 30){
+
+            Console.WriteLine("O A É MENOR QUE 20 OU É IGUAL A TRINTA");
+
+           }else{
+
+            Console.WriteLine("O A É MAIOR QUE 20");
+        }
 
 
     Console.WriteLine("pressione qualquer tecla para continuar");
