5f1570d45e2cf4dbe7eef365e63f791f45f714eb
Marcelo H <73535877+feitan02@users.noreply.github.com>
2021-12-20 13:37:51 -0300

    Create usando For.cs

---
+++ usando For.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Globalization;
+
+class arquivo1{
+
+ public static void Main(){
+
+    for(int a = 1; a <=10; a++){
+
+            Console.WriteLine("Numero = " + a);
+
+        }
+
+
+
+    Console.WriteLine("pressione qualquer tecla para continuar");
+    Console.ReadKey();
+
+
+
+}
+} 
