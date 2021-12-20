e17aee418c98b41cc59d68a80d6d52fd6aa2ea64
Marcelo H <73535877+feitan02@users.noreply.github.com>
2021-12-20 11:57:58 -0300

    Update inicio.cs

--- inicio.cs
+++ inicio.cs
@@ -7,18 +7,30 @@ class arquivo1{
 
      Console.WriteLine("Estrutura base C#");
 
-     Console.WriteLine("Digite um numero, um caracter e um numero quebrado");
+     Console.WriteLine("Digite um numero, um caracter e um numero quebrado: ");
      int n1 = int.Parse(Console.ReadLine());
 
      char ch = char.Parse(Console.ReadLine());
 
      double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
+    Console.WriteLine("Digite um nome, sexo, idade e altura");
+    String[] vetor = Console.ReadLine().Split(' ');
+    String nome = vetor[0];
+    char sexo = char.Parse(vetor[1]);
+    int idade = int.Parse(vetor[2]);
+    double altura = double.Parse(vetor[3]);
+
+
+
      Console.WriteLine("Voce digitou: ");
      Console.WriteLine(n1);
      Console.WriteLine(ch);
-     Console.WriteLine(n2);
-
+     Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
+     Console.WriteLine(nome);
+     Console.WriteLine(sexo);
+     Console.WriteLine(idade);
+     Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
 
 
     Console.WriteLine("pressione qualquer tecla para continuar");
