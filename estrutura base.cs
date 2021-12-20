0bb128d65fe922c8bb9a8bfda46b06b668b76f5b
Marcelo H <73535877+feitan02@users.noreply.github.com>
2021-12-20 12:09:48 -0300

    Create estrutura base.cs

---
+++ estrutura base.cs
@@ -0,0 +1,17 @@
+Using System;
+
+class arquivo1{
+
+ public static void Main(){
+
+     Console.WriteLine("Estrutura base C#");
+
+
+    Console.WriteLine("pressione qualquer tecla para continuar");
+    Console.ReadKey();
+
+
+
+}
+} 
+estrutura base
