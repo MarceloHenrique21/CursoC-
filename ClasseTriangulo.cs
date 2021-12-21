3fa02634e7b5fbb3b38c8260e1e39b68e71ae43f
Marcelo H <73535877+feitan02@users.noreply.github.com>
2021-12-21 11:13:07 -0300

    Create ClasseTriangulo.cs
    Classe usanda para calcular triangulo com OO

---
+++ ClasseTriangulo.cs
@@ -0,0 +1,13 @@
+
+
+namespace arquivo4_inicio_programação_orientada_a_objetos{
+    class Triangulo {
+        
+
+        public double A;
+        public double B;
+        public double C;
+
+
+    }
+}
